5c18e36df426253119d309f891be72e4347c4420
jasoncaldwell1998 <86395236+jasoncaldwell1998@users.noreply.github.com>
2021-07-21 08:51:53 +0530

    Add files via upload

---
+++ Khush/scrape_sorted.py
@@ -0,0 +1,89 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
+from telethon.tl.functions.channels import InviteToChannelRequest
+import sys
+import csv
+import traceback
+import time
+from datetime import datetime
+api_id = 2410047
+api_hash = '0808c8985e03f6eb6df122b1a27592d8'
+phone = +919443546972
+client = TelegramClient(phone, api_id, api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code recieved to your Telegram messenger: '))
+
+
+chats = []
+last_date = None
+chunk_size = 200
+groups=[]
+
+result = client(GetDialogsRequest(
+             offset_date=last_date,
+             offset_id=0,
+             offset_peer=InputPeerEmpty(),
+             limit=chunk_size,
+             hash = 0
+         ))
+chats.extend(result.chats)
+
+for chat in chats:
+    try:
+        if chat.megagroup== True:
+            groups.append(chat)
+    except:
+        continue
+
+print('Choose a group to scrape members from:')
+i=0
+for g in groups:
+    print(str(i) + '- ' + g.title)
+    i+=1
+
+g_index = input("Enter a Number: ")
+target_group=groups[int(g_index)]
+
+print('Fetching Members...')
+all_participants = []
+all_participants = client.get_participants(target_group, aggressive=True)
+
+print('Saving In file...')
+with open("E:\Telegram Scripts Bundle\Scripts\members_Sorted.csv","w",encoding='UTF-8') as f:
+    writer = csv.writer(f,delimiter=",",lineterminator="\n")
+    writer.writerow(['username','user id', 'access hash','name','group', 'group id','last seen'])
+    for user in all_participants:
+        accept=True
+        try:
+            lastDate=user.status.was_online
+            num_months = (datetime.now().year - lastDate.year) * 12 + (datetime.now().month - lastDate.month)
+            if(num_months>1):
+                accept=False
+        except:
+            continue
+
+        if (accept) :
+            if user.username:
+                username= user.username
+            else:
+                username= ""
+            if user.first_name:
+                first_name= user.first_name
+            else:
+                first_name= ""
+            if user.last_name:
+                last_name= user.last_name
+            else:
+                last_name= ""
+            name= (first_name + ' ' + last_name).strip()
+            writer.writerow([username,user.id,user.access_hash,name,target_group.title, target_group.id,user.status])
+
+print('Members scraped successfully.')
